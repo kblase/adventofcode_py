5aa5b22c9291206a4d301dfbe5a8ea719ac49191
doktorwho <doktorwho@flachderplatte.de>
2021-12-01 20:21:40 +0100

    fixed of by one error key was comparing integers not strings added second way to solve

---
+++ Day 1/sonar_sweep.py
@@ -0,0 +1,26 @@
+def read_measurements():
+  global measurements
+  measurements=open('measurements').readlines()
+  measurements=[line.rstrip() for line in measurements]
+
+def find_increases():
+  go_deeper_list=[]
+  for i in range(1,len(measurements)):
+    if int(measurements[i]) > int(measurements[i-1]):
+      go_deeper_list.append(measurements[i])
+  print('Solved by adding Elements and get length of list')
+  print('The Water depth increases', len(go_deeper_list), 'times!')
+  print('------------------------')
+
+def find_increases_add():
+  go_deeper=0
+  for i in range(1,len(measurements)):
+    if int(measurements[i]) > int(measurements[i-1]):
+      go_deeper+= 1
+  print('Solved by adding Numbers')
+  print('The Water depth increases', go_deeper, 'times!')
+  print('------------------------')
+
+read_measurements()
+find_increases()
+find_increases_add()
