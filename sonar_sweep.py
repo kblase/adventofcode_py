c8370bd2dfde13498dfd0c9d16aa1338328cf336
doktorwho <doktorwho@flachderplatte.de>
2021-12-01 19:34:39 +0100

    solved puzzle with static file Copy pasted file to local and solved it

--- sonar_sweep.py
+++ sonar_sweep.py
@@ -1,5 +1,11 @@
-with open('measurements') as my_file:
-  measurements_array=my_file.readlines()
+# with open('measurements') as my_file:
+go_deeper= []
+measurements=open('measurements').readlines()
+measurements=[line.rstrip() for line in measurements]
 
-print(measurements_array)
-print('test')
+print(len(measurements))
+for i in range(0,len(measurements)):
+  if measurements[i] > measurements[i-1]:
+    go_deeper.append(1)
+
+print('The Water depth increases', len(go_deeper), 'times!')
