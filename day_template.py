3711ed7e09dead3b22befdd69d2b3b5e5d07a2ec
Kevin Blase <kevin.blase@computacenter.com>
2022-12-02 15:07:35 +0100

    change vars in template make automatic casehandling for data_input add argparse to trigger use of exampledata manually

--- day_template.py
+++ day_template.py
@@ -5,21 +5,22 @@ current_day = ""
 
 def read_input(input_file: Path):
     with open(input_file, "r") as data:
-        content = [line.strip() for line in data]
-    return content
+        lines = [line.strip() for line in data]
+    return lines
 
 
-def puzzle_a(content):
+def puzzle_a(input):
     return
 
 
-def puzzle_b(content):
+def puzzle_b(input):
     return
 
 
 if __name__ == "__main__":  # pragma: no cover
     print(f"Day {current_day}")
-    input_file = Path(__file__).parent / f"data_input/day{current_day}.txt"
-    content = read_input(input_file)
-    print("Puzzle a:", puzzle_a(content))
-    print("Puzzle b:", puzzle_b(content))
+    input_file = Path(__file__).parent / f"data_input/day{current_day}_ex.txt"
+    # input_file = Path(__file__).parent / f"data_input/day{current_day}.txt"
+    # print(read_input(input_file))
+    print("Puzzle a:", puzzle_a(read_input(input_file)))
+    print("Puzzle b:", puzzle_b(read_input(input_file)))
