5d02438e7d9e4a64723535fca626aa99fedd0f36
Kevin Blase <kevin.blase@computacenter.com>
2022-12-02 16:23:22 +0100

    solve day2 and cleanup

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from argparse import ArgumentParser
 
 current_day = "2"
 
@@ -16,10 +17,11 @@ points ={
     "won": 6
 }
 
-def read_input(input_file: Path):
-    with open(input_file, "r") as data:
-        content = [line.strip() for line in data]
-    games = [game.split() for game in content]
+
+def read_input(filename):
+    with open(Path(__file__).parent/f"data_input/{filename}.txt", "r") as data:
+        lines = [line.strip() for line in data]
+    games = [game.split() for game in lines]
     return games
 
 
@@ -51,11 +53,21 @@ def puzzle_b(content):
             score += rps[round[0]]-2 + points["won"]
     return score
 
+def main():
+    parser = ArgumentParser(
+        description="Löse Advend of Code Aufgaben")
+    parser.add_argument('-e','--example', action='store_true', help="Soll der Beispiel Datensatz benutzt werden?")
+    args = parser.parse_args()
+    print(f"Day {current_day}")
+    filename = f"day{current_day}"
+    if Path.is_file(Path(__file__).parent/f"data_input/{filename}.txt") and (not args.example):
+        print(f"Using real Data!")
+    else:
+        filename = f"day{current_day}_ex"
+        print("The Example Data looks like: \n")
+        print(f"{read_input(filename)}\n")
+    print("Puzzle a:", puzzle_a(read_input(filename)))
+    print("Puzzle b:", puzzle_b(read_input(filename)))
 
 if __name__ == "__main__":  # pragma: no cover
-    print(f"Day {current_day}")
-    # input_file = Path(__file__).parent / f"data_input/day{current_day}_ex.txt"
-    input_file = Path(__file__).parent / f"data_input/day{current_day}.txt"
-    content = read_input(input_file)
-    print("Puzzle a:", puzzle_a(content))
-    print("Puzzle b:", puzzle_b(content))
+    main()
