366fdab01df1c194df73a7dd8f9475eeb4049487
Kevin Blase <kevin.blase@computacenter.com>
2022-12-02 15:39:06 +0100

    implement main()

--- day_template.py
+++ day_template.py
@@ -1,13 +1,13 @@
 from pathlib import Path
+from argparse import ArgumentParser
 
 current_day = ""
 
-
-def read_input(input_file: Path):
-    with open(input_file, "r") as data:
+def read_input(filename):
+    with open(Path(__file__).parent/f"data_input/{filename}.txt", "r") as data:
         lines = [line.strip() for line in data]
+    # put data formatting here
     return lines
-
 
 def puzzle_a(input):
     return
@@ -16,11 +16,21 @@ def puzzle_a(input):
 def puzzle_b(input):
     return
 
+def main():
+    parser = ArgumentParser(
+        description="Löse Advend of Code Aufgaben")
+    parser.add_argument('-e','--example', action='store_true', help="Toggles Example Dataset manually")
+    args = parser.parse_args()
+    print(f"Day {current_day}")
+    filename = f"day{current_day}"
+    if Path.is_file(Path(__file__).parent/f"data_input/{filename}.txt") and (not args.example):
+        print(f"Using real Data!")
+    else:
+        filename = f"day{current_day}_ex"
+        print("The Example Data looks like: \n")
+        print(f"{read_input(filename)}\n")
+    print("Puzzle a:", puzzle_a(read_input(filename)))
+    print("Puzzle b:", puzzle_b(read_input(filename)))
 
 if __name__ == "__main__":  # pragma: no cover
-    print(f"Day {current_day}")
-    input_file = Path(__file__).parent / f"data_input/day{current_day}_ex.txt"
-    # input_file = Path(__file__).parent / f"data_input/day{current_day}.txt"
-    # print(read_input(input_file))
-    print("Puzzle a:", puzzle_a(read_input(input_file)))
-    print("Puzzle b:", puzzle_b(read_input(input_file)))
+    main()
