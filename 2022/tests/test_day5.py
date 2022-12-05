92600e0abee3888693c3be917ca23283f405ea83
Kevin Blase <kevin.blase@computacenter.com>
2022-12-05 16:24:49 +0100

    add test for input values

--- 2022/tests/test_day5.py
+++ 2022/tests/test_day5.py
@@ -2,6 +2,10 @@ from day5 import puzzle_a, puzzle_b, read_input
 
 current_day = "5"
 
+def test_input_return_values():
+    filename = f"day{current_day}_ex"
+    assert read_input(filename) == ([['Z', 'N'], ['M', 'C', 'D'], ['P']], [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]])
+
 def test_puzzle_a_example():
     filename = f"day{current_day}_ex"
     assert puzzle_a(read_input(filename)) == 'CMZ'
