cbe0c6c373bb6440c136d477146e7f6a92c56be8
doktorwho <doktorwho@flachderplatte.de>
2022-12-05 23:52:29 +0100

    solve day 6 2021

---
+++ 2021/day6.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from argparse import ArgumentParser
+import collections
+
+current_day = 6
+
+
+def read_input(filename):
+    with open(Path(__file__).parent / f"data_input/{filename}.txt", "r") as data:
+        lines = [line.strip() for line in data]
+    line = list(map(int, lines[0].split(",")))
+    return line
+
+
+def puzzle_a(input):
+    for i in range(80):
+        new_fish = 0
+        for l in range(len(input)):
+            input[l] -= 1
+            if input[l] < 0:
+                new_fish += 1
+                input[l] = 6
+        for new in range(new_fish):
+            input.append(8)
+    return len(input)
+
+
+def puzzle_b(input):
+    fish_totals = collections.deque(input.count(i) for i in range(9))
+    for _ in range(256):
+        fish_totals.rotate(-1)
+        fish_totals[6] += fish_totals[8]
+    return sum(fish_totals)
+
+
+def main():
+    parser = ArgumentParser(description="Löse Advend of Code Aufgaben")
+    parser.add_argument(
+        "-e", "--example", action="store_true", help="Toggles Example Dataset manually"
+    )
+    args = parser.parse_args()
+    print(f"Day {current_day}")
+    filename = f"day{current_day}"
+    if Path.is_file(Path(__file__).parent / f"data_input/{filename}.txt") and (
+        not args.example
+    ):
+        print(f"Using real Data!")
+    else:
+        filename = f"day{current_day}_ex"
+        print("The Example Data looks like: \n")
+        print(f"{read_input(filename)}\n")
+    print("Puzzle a:", puzzle_a(read_input(filename)))
+    print("Puzzle b:", puzzle_b(read_input(filename)))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
